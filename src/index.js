0ba4aee5f124554c83ab9a43f37619c9340f7007
LuqmanKhan <luqmnkn@gmail.com>
2026-03-26 16:30:07 +0500

    routes and controller and debugging

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import { app }  from "./app.js"
 import { connectDB } from "./db/index.js"
 // ('dotenv').config({path: './env'})
 import dotenv from "dotenv"
+
 
 dotenv.config({
     path:'./.env',
